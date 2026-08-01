5486b6e1318092f012a9f97e433428c21d2c5443
dufi1984 <dufi1984@users.noreply.github.com>
2026-08-01 20:21:37 +0200

    Enable universal accept='*/*' input filter to show full Android system choice screen (Camera / Gallery / Files / Drive) upon tapping Tallózás

--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -64,13 +64,13 @@ export default function FilePicker({ lang, files, setFiles }) {
   return (
     <div className="w-full bg-zinc-900/90 rounded-2xl p-4 sm:p-5 flex flex-col gap-3.5 border border-zinc-800 shadow-xl transition-all">
       
-      {/* Strict Photo & Video Gallery Input */}
+      {/* Universal Input enabling Android System Choice Screen (Camera / Gallery / Files / Drive) */}
       <input
         type="file"
         ref={inputRef}
         onChange={handleFileChange}
         multiple
-        accept="image/*,video/*"
+        accept="*/*"
         className="hidden"
       />
 
