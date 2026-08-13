2377ca01bdb0b282baa9409902b91fd8769c8238
dufi1984 <dufi1984@users.noreply.github.com>
2026-08-13 23:30:44 +0200

    feat: add animated 5s glowing border beam to header matching Ant Design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,6 +51,9 @@ export default function Header({ isConnected, onOpenQr, onForceReload, isRefresh
         </div>
 
       </div>
+
+      {/* Ant Design Animated Glow Beam on bottom border */}
+      <div className="ant-header-beam" />
     </header>
   );
 }
